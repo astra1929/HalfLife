418483c72a2c738ea823f85620044d372ea1ffd7
astra1929 <138461804+astra1929@users.noreply.github.com>
2024-04-27 02:38:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 function halfLife() {
     let ran = Math.round(Math.random());
     let res = document.getElementById("result")
+    let arr1 = document.getElementById("arrow1")
+    let arr2 = document.getElementById("arrow2")
     if (ran === 1){
-        result.innerHTML = "Атом распался"
-        result.style.border = "3px solid #0d8531"
-        result.style.background = "#0d8531"
+        res.innerHTML = "атом распался"
+        res.style.border = "3px solid #0d8531"
+        res.style.background = "#0d8531"
+        res.style.display = "inline-block"
+        arr1.style.display = "inline-block"
+        arr2.style.display = "none"
     } else {
-        result.innerHTML = "Атом не распался"
-        result.style.border = "3px solid #be1212"
-        result.style.background = "#be1212"
+        res.innerHTML = "атом не распался"
+        res.style.border = "3px solid #be1212"
+        res.style.background = "#be1212"
+        res.style.display = "inline-block"
+        arr2.style.display = "inline-block"
+        arr1.style.display = "none"
     }
 }
